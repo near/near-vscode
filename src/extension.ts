5330471552cece4d16da62ee2b0436113f87a2b3
Max <max.khrp@gmail.com>
2023-02-01 17:02:55 +0200

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { WidgetsFS } from './fs-provider';
+import {NearFS, WidgetAccountDir} from './fs-provider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const widgetsFS = new WidgetsFS();
-	context.subscriptions.push(vscode.workspace.registerFileSystemProvider('nearfs', widgetsFS, { isCaseSensitive: true }));
-	
-	context.subscriptions.push(vscode.commands.registerCommand('nearfs.workspaceInit', _ => {
-		// widgetsFS.init();
-		vscode.workspace.updateWorkspaceFolders(0, null, { uri: vscode.Uri.parse('nearfs:/'), name: "NEAR.social" });
-	}));
+  const widgetsFS = new NearFS();
+  context.subscriptions.push(vscode.workspace.registerFileSystemProvider('nearfs', widgetsFS, {isCaseSensitive: true}));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsnear1" is now active!');
+  context.subscriptions.push(vscode.commands.registerCommand('nearfs.workspaceInit', _ => {
+    vscode.workspace.updateWorkspaceFolders(0, null, {uri: vscode.Uri.parse('nearfs:/'), name: "NEAR.social"});
+  }));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsnear1.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsnear1!');
-	});
+  context.subscriptions.push(vscode.commands.registerCommand('nearfs.openWidgetsFromAccount', async () => {
+    const accountId = await vscode.window.showInputBox({ placeHolder: 'mainnet account id' });
+    if (accountId) {
+      widgetsFS.loadAccount(accountId);
+      widgetsFS.root.entries.set(accountId, new WidgetAccountDir(accountId));
+    }
 
-	context.subscriptions.push(disposable);
+  }));
+
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vsnear1" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('vsnear1.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from vsnear1!');
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
--- src/near-bridge.ts
+++ src/near-bridge.ts
@@ -1,16 +1,20 @@
+import {Near} from 'near-api-js';
+
 const { providers } = require("near-api-js");
 const provider = new providers.JsonRpcProvider("https://rpc.near.org");
 
-let widgets: NearWidget[] | null = null;
-
+export type AccountId = string;
 export interface NearWidget {
   name: string;
   code: string;
 }
 
-export const getWidgetsFromChain = async (accountId: string): Promise<any> => {
-  if (widgets !== null) {
-    return;
+let widgets: Record<AccountId, NearWidget[]> = {};
+
+
+export const getWidgetsFromChain = async (accountId: string, forceRefresh = false): Promise<NearWidget[]> => {
+  if (!forceRefresh && widgets[accountId]) {
+    return widgets[accountId];
   }
   const args = Buffer.from(`{"keys":["${accountId}/widget/**"]}`).toString(
     "base64"
@@ -27,32 +31,10 @@ export const getWidgetsFromChain = async (accountId: string): Promise<any> => {
   const w: NearWidget[] = Object.entries(rawWidgets).map(
     ([name, data]: [string, any]) => ({
       name,
-      code: typeof data === "object" ? data[""] : "ERROR_GETTING_WIDGET_CODE",
+      code: typeof data === "object" ? data[""] : "//",
     })
   );
   console.log(w);
-  widgets = w;
-};
-
-export const getAccountWidgetsNames = async (
-  accountId: string
-): Promise<string[]> => {
-  await getWidgetsFromChain(accountId);
-  if (widgets !== null) {
-    return widgets.map((w) => w.name);
-  } else {
-    return [];
-  }
-};
-
-export const readWidgetCode = async (
-  accountId: string,
-  widgetName: string
-): Promise<string | null> => {
-  await getWidgetsFromChain(accountId);
-  if (widgets !== null) {
-    return widgets.find((w) => w.name === widgetName)?.code || null;
-  } else {
-    return null;
-  }
+  widgets[accountId] = w;
+  return widgets[accountId];
 };
